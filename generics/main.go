69b4df89f4611fb0f5cabf03ca0f661161af7ead
akashgupta <akash5gupta5@gmail.com>
2024-05-07 09:55:28 +0530

    git add generics

--- generics/main.go
+++ generics/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
 
 /**
 	- Type parameter
@@ -22,6 +26,21 @@ func second[T minTypes](a T, b T) T {
 	return b
 }
 
+// generics in structs
+type CustomData interface {
+	constraints.Ordered | []string
+}
+
+type User[T CustomData] struct {
+	id   int
+	name string
+	data T
+}
+
+type CustomMap[T comparable, V int | string] map[T]V
+
+// comparable are types which can be compared using == operator
+
 func main() {
 	// fmt.Println(first[int](1, 2))
 	fmt.Println(first[float64](1.0, 2))
@@ -30,4 +49,17 @@ func main() {
 	fmt.Println(first(1.0, 2.0))
 
 	// ~sub types of type
+
+	// user struct
+	u := User[float64]{
+		id:   0,
+		name: "akash",
+		data: 12.2,
+	}
+
+	fmt.Println("user: ", u)
+
+	d := CustomMap[string, int]{}
+
+	d["akash"] = 3
 }
